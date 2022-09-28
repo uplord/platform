eab537209057284e6a67947d604f402d77069cbb
Debian <bitnami@ip-172-26-2-202.eu-west-2.compute.internal>
2022-09-28 12:52:14 +0000

    style basics

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Platform</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="robots" content="noindex, nofollow">
+        <link rel="stylesheet" href="assets/min/style.css?ver=<?= dechex(time()) ?>" type="text/css" media="all">
+    </head>
+    <body>
+        <main>
+            <section>
+                <div class="inner-container">
+                    <h1>Homepage</h1>
+                </div>
+            </section>
+        </main>
+        <script type="text/javascript" src="assets/min/index.js?ver=<?= dechex(time()) ?>"></script>
+    </body>
+</html>
